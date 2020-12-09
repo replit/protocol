d9efe017512f045048513558e989c48bb2804cfb
lhchavez <lhchavez@lhchavez.com>
2020-12-09 10:35:21 -0800

    Added a small tool to fix the JSDoc declarations to add some flexibility
    This still emits completely sound types (needed for Flow, since
    TypeScript is intentionally unsound), and also lets the callers do more
    idiomatic protobuf construction.

---
+++ js/tools/fixup.js
@@ -0,0 +1,136 @@
+#!/usr/bin/env node
+/**
+ * @file A tool that fixes the pbjs-generated JSDoc annotations so that it can
+ * produce sound TypeScript and Flow type declarations.
+ * @flow
+ */
+'use strict';
+
+const fs = require('fs');
+
+// The regex that matches a JSDoc comment.
+const commentRegex = new RegExp('/\\*\\*([^*]|\\*[^/])*\\*/', 'smg');
+
+// The regex that matches an `@interface` JSDoc line.
+const interfaceRegex = new RegExp('@interface\\s+(\\S+)', 'g');
+
+// The regex that matches an `@implements` JSDoc line.
+const implementsRegex = new RegExp('.*@implements\\b.+\n');
+
+// The regex that matches a `@property` JSDoc line.
+const propertyRegex = new RegExp('@property\\s+\\{([^}]+)\\}', 'g');
+
+// The regex that matches a proto repeated field type.
+const arrayTypeRegex = new RegExp('^Array\\.<(.+)>$');
+
+// The regex that matches a proto map field type.
+const mapTypeRegex = new RegExp('^Object\\.<string,(.+)>$');
+
+/**
+ * Fixes a property type in an interface declaration.
+ *
+ * This does one of the following transformations:
+ *
+ * - For primitive types (number, boolean, etc.), it leaves the type as-is.
+ * - For repeated types, it recursively fixes the type of the elements of the
+ *   array.
+ * - For map types, it recursively fixes the type of the values of the map.
+ * - For message types, it allows both the concrete message (with all the
+ *   fields filled in) as well as the abstract interface (that allows for all
+ *   fields to be optional, in accordance with proto3 semantics).
+ *
+ * @param {string} typeName the name of the property.
+ * @param {Set<string>} interfaces the declared interfaces in this file
+ * @returns {string} the fixed property type.
+ */
+function fixPropertyType(typeName, interfaces) {
+  switch (typeName) {
+    case 'number':
+    case 'boolean':
+    case 'string':
+    case 'Uint8Array':
+    case 'null':
+      return typeName;
+  }
+
+  // proto repeated fields.
+  let match = arrayTypeRegex.exec(typeName);
+  if (match !== null) {
+    return `Array.<${fixPropertyType(match[1], interfaces)}>`;
+  }
+
+  // proto map fields.
+  match = mapTypeRegex.exec(typeName);
+  if (match !== null) {
+    return `Object.<string,${fixPropertyType(match[1], interfaces)}>`;
+  }
+
+  // This is now a dot-separated fully qualified type name.
+  const tokens = typeName.split('.');
+  tokens[tokens.length - 1] = 'I' + tokens[tokens.length - 1];
+  const interfaceName = tokens.join('.');
+  if (!interfaces.has(interfaceName)) {
+    return typeName;
+  }
+  return `${typeName}|${tokens.join('.')}`;
+}
+
+/**
+ * Fixes JSDoc declarations. For concrete message declarations, it removes
+ * @implements annotations that break the soundness of the type system, and for
+ * interface message declarations, it allows for all message types to be either
+ * the concrete message type or the abstract interface one.
+ *
+ * @param {string} jsDoc the contents of a JSDoc
+ * @param {Set<string>} interfaces the declared interfaces in this file
+ * @returns {string} the fixed JSDoc.
+ */
+function fixJsDoc(jsDoc, interfaces) {
+  if (jsDoc.indexOf('@interface') !== -1) {
+    // This is an interface declaration.
+    jsDoc = jsDoc.replace(propertyRegex, (match, property) => {
+      const options = property
+        .split('|')
+        .map((property) => fixPropertyType(property, interfaces))
+        .join('|');
+      return `@property {${options}}`;
+    });
+  } else {
+    // This is a class declaration.
+
+    // Remove any @implements annotations.
+    jsDoc = jsDoc.replace(implementsRegex, '');
+  }
+  return jsDoc;
+}
+
+exports.main = function (args /*: string[] */) {
+  const stdinBuffer = fs.readFileSync(0, 'utf-8');
+
+  const interfaces /*: Set<string> */ = new Set();
+  while (true) {
+    const match = interfaceRegex.exec(stdinBuffer);
+    if (match === null) {
+      break;
+    }
+    interfaces.add(match[1]);
+  }
+
+  const chunks = [];
+
+  let lastIndex = 0;
+  while (true) {
+    const match = commentRegex.exec(stdinBuffer);
+    if (match === null) {
+      break;
+    }
+    chunks.push(stdinBuffer.slice(lastIndex, match.index));
+    chunks.push(fixJsDoc(match[0], interfaces));
+    lastIndex = match.index + match[0].length;
+  }
+  chunks.push(stdinBuffer.slice(lastIndex));
+
+  fs.writeSync(1, chunks.join(''));
+};
+
+exports.main(process.argv);
