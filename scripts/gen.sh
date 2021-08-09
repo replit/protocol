8f768d5f2a82c8ba8310f666d105547181353323
Wade Bourne <wadeabourne@gmail.com>
2021-08-09 16:59:18 -0500

    make requested changes

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-PATH=$PATH:node_modules/.bin
+PATH="${PATH}:node_modules/.bin"
 
 set -e
 
-if [[ -z $TARGET ]]; then 
+if [[ -z "${TARGET}" ]]; then 
     TARGET="./index"
 fi
 
@@ -12,23 +12,23 @@ pbjs \
     --force-message \
     -t static-module \
     -w default \
-    -o $TARGET.js \
+    -o "${TARGET}".js \
     $@
 
-./tools/jsdoc.js <$TARGET.js >$TARGET.tmp.js
+./tools/jsdoc.js <"${TARGET}.js" >"${TARGET}.tmp.js"
 
-mv $TARGET.tmp.js $TARGET.js
+mv "${TARGET}.tmp.js" "${TARGET}.js"
 
-pbts -o $TARGET.d.ts $TARGET.js
+pbts -o "${TARGET}.d.ts" "${TARGET}.js"
 
-sed -i.bak 's/constructor(/private constructor(/' $TARGET.d.ts
+sed -i.bak 's/constructor(/private constructor(/' "${TARGET}.d.ts"
 
-rm -f $TARGET.d.ts.bak
+rm -f "${TARGET}.d.ts.bak"
 flowgen \
     --interface-records \
     --no-inexact \
     --add-flow-header \
-    --output-file $TARGET.js.flow $TARGET.d.ts
+    --output-file "${TARGET}.js.flow" "${TARGET}.d.ts"
 
-sed -i.bak 's/declare var api: typeof npm\\$namespace\\$api;/export var api = npm$namespace$api;/' $TARGET.js.flow
-rm -f $TARGET.js.flow.bak
+sed -i.bak 's/declare var api: typeof npm\$namespace\$api;/export var api = npm$namespace$api;/' "${TARGET}.js.flow"
+rm -f "${TARGET}.js.flow.bak"
