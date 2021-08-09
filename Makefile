23d42de8fbb67eed950878b959bd72a4f4921b6d
AllAwesome497 <47748690+AllAwesome497@users.noreply.github.com>
2021-08-09 17:08:33 -0500

    Add protobuf definitions for V2 of I/O testing (#27)
    * Add protobuf definitions for V2 of I/O testing
    
    * fix comment
    
    * make requested changes

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 bump-%:
 	OLD_VERSION=$$(git tag | sort -r --version-sort | head -n1) && \
-	SEMVER_NEW_TAG=$$(bash ./semver.sh bump $(*) $${OLD_VERSION}) && \
+	SEMVER_NEW_TAG=$$(bash ./scripts/semver.sh bump $(*) $${OLD_VERSION}) && \
 	SEMVER_NEW_TAG="v$${SEMVER_NEW_TAG}" && \
 	echo "new version: $${SEMVER_NEW_TAG}" && \
 	cd lib && npm version $${SEMVER_NEW_TAG} && \
@@ -11,7 +11,7 @@ bump-%:
 all: build bump-patch publish
 
 clean:
-	rm -f lib/index.d.ts lib/index.js lib/export.flow.js
+	rm -f lib/index.d.ts lib/index.js lib/export.flow.js lib/iotester/*
 build: clean
 	cd lib && npm install && npm run prepublishOnly
 publish:
---
+++ scripts/gen.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+PATH="${PATH}:node_modules/.bin"
+
+set -e
+
+if [[ -z "${TARGET}" ]]; then 
+    TARGET="./index"
+fi
+
+pbjs \
+    --force-number \
+    --force-message \
+    -t static-module \
+    -w default \
+    -o "${TARGET}".js \
+    $@
+
+./tools/jsdoc.js <"${TARGET}.js" >"${TARGET}.tmp.js"
+
+mv "${TARGET}.tmp.js" "${TARGET}.js"
+
+pbts -o "${TARGET}.d.ts" "${TARGET}.js"
+
+sed -i.bak 's/constructor(/private constructor(/' "${TARGET}.d.ts"
+
+rm -f "${TARGET}.d.ts.bak"
+flowgen \
+    --interface-records \
+    --no-inexact \
+    --add-flow-header \
+    --output-file "${TARGET}.js.flow" "${TARGET}.d.ts"
+
+sed -i.bak 's/declare var api: typeof npm\$namespace\$api;/export var api = npm$namespace$api;/' "${TARGET}.js.flow"
+rm -f "${TARGET}.js.flow.bak"
